90e35ea495752cba851f05faafb5e7cbb2daff17
Gmo298 <gavinmorris298@gmail.com>
2025-07-26 20:59:35 -0500

    gtd face cards

--- main.lua
+++ main.lua
@@ -272,3 +272,74 @@ TexturePack{
 		}
 	}
 }
+
+local suits = {'hearts', 'clubs', 'diamonds', 'spades'}
+local ranks_full = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'}
+local ranks_num = {'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
+local ranks_face = {'Jack', 'Queen', 'King'}
+local display_ranks = {'Ace', 'King', 'Queen', 'Jack', '10'}
+
+function style_map(contrast, suit_idx, flavor)
+    local map = {}
+
+    for idx, rank in ipairs(ranks_full) do
+        map[rank] = {
+            atlas = 'gtd-full-'..contrast,
+            pos = {x = idx - 1, y = suit_idx - 1}
+        }
+    end
+
+    if flavor == 'k1-ace' then
+        for idx, rank in ipairs(ranks_num) do
+            map[rank] = {
+                atlas = 'gtd-k1ace-'..contrast,
+                pos = {x = idx - 1, y = suit_idx - 1}
+            }
+        end
+    end
+
+    return map
+end
+
+
+-- generate atlases
+local atlas_path = {
+    {'gtd-Full-LC.png', 'gtd-full-lc'},
+    {'gtd-Full-HC.png', 'gtd-full-hc'}
+}
+for _, x in ipairs(atlas_path) do
+    SMODS.Atlas{
+        key = x[2],
+        px = 71,
+        py = 95,
+        path = x[1],
+        prefix_config = {key = false},
+    }
+end
+
+-- generate entries
+for idx, suit in ipairs(suits) do
+    SMODS.DeckSkin{
+        key = suit.."_skin",
+        suit = suit:gsub("^%l", string.upper),
+        loc_txt = { ['en-us'] = "GTD" },
+        palettes = {{
+                key = 'faceonly-lc',
+                ranks = ranks_face,
+                display_ranks = display_ranks,
+                atlas = 'gtd-full-lc',
+                posStyle = style_map('lc', idx, 'face-only'),
+                loc_txt = {['en-us'] = "Kaleb Face Cards"},
+            },
+            {
+                key = 'faceonly-hc',
+                ranks = ranks_face,
+                display_ranks = display_ranks,
+                atlas = 'gtd-full-lc',
+                posStyle = style_map('hc', idx, 'face-only'),
+                loc_txt = {['en-us'] = "GTD Face Cards"},
+                hc_default = true
+            }
+        }
+    }
+end
